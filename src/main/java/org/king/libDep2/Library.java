43b5f850067b0d4cb54906a982e685e92cd548f9
Steven Cocks <ext.steven.cocks@king.com>
2024-03-19 14:28:56 +0100

    Add unused methods

--- src/main/java/org/king/libDep2/Library.java
+++ src/main/java/org/king/libDep2/Library.java
@@ -1,13 +1,18 @@
 package org.king.libdep2;
 public class Library {
-    public String[] count(Integer number) {
-        String[] lines = new String[number];
-        for (int i = 0; i < number; i++) {
-            lines[i] = "j = " + i;
-        }
-        return lines;
-    }
     public String greet(String name) {
-        return "Greetings, " + name + "!";
+        return "greet from libdep2, " + name + "!";
+    }
+    public String unused1(String name) {
+        return "unused1 from libdep2, " + name + "!";
+    }
+    public String unused2(String name) {
+        return "unused2 from libdep2, " + name + "!";
+    }
+    public String unused3(String name) {
+        return "unused3 from libdep2, " + name + "!";
+    }
+    public String unused4(String name) {
+        return "unused5 from libdep2, " + name + "!";
     }
 }
--- src/test/java/org/king/libDep2/MainTest.java
+++ src/test/java/org/king/libDep2/MainTest.java
@@ -4,20 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MainTest {
-    @Test
-    void testCount() {
-        Library library = new Library();
-        String[] result = library.count(20);
-        assertEquals(20, result.length);
-        for (int i = 0; i < 20; i++) {
-            assertEquals("j = " + i, result[i]);
-        }
-    }
+class MainTest {   
     @Test
     void testGreet() {
         Library library = new Library();
         String result = library.greet("lib-dep-2");
-        assertEquals("Greetings, lib-dep-2!", result);
+        assertEquals("greet from libdep2, lib-dep-2!", result);
     }
 }
